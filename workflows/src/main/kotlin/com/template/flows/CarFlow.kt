88d8bf81acdc38d5c8aca66f6d769df69fd0e19e
Bruno <brunofc11@gmail.com>
2020-01-28 17:07:19 -0300

    Ready to test

--- workflows/src/main/kotlin/com/template/flows/CarFlow.kt
+++ workflows/src/main/kotlin/com/template/flows/CarFlow.kt
@@ -27,11 +27,12 @@ class CarIssueInitiator(
         var make : String,
         var model : String,
         var dealershipLocation : String
-) : FlowLogic<Unit>() {
-    override val progressTracker = ProgressTracker()
+) : FlowLogic<SignedTransaction>() {
+    //When i change the Flowgic generic parameter a going to return a type of this generic in my
+    //overrited function
 
     @Suspendable
-    override fun call() {
+    override fun call() : SignedTransaction{
         // Initiator flow logic goes here.
         val notary = serviceHub.networkMapCache.notaryIdentities.first()
 
@@ -45,13 +46,44 @@ class CarIssueInitiator(
                         licensePlateNumber, make, model, dealershipLocation,
                         UniqueIdentifier())
 
+        val txBuilder = TransactionBuilder(notary)
+        //when you add an outputstate you also need to send the contract itself
+        txBuilder.addOutputState(carState, CarContract.ID)
+        txBuilder.addCommand(issueCommand)
+
+        //You have to verify the transactions in this commandLine
+        txBuilder.verify(serviceHub)
+
+        //Here we can sign the initial transaction
+        val tx = serviceHub.signInitialTransaction(txBuilder)
+
+        //creating an flow for every node in the transaction excluding the sending node
+        val sessions = (carState.participants - ourIdentity).map { initiateFlow(it as Party) }
+
+        //create a subflow to collect the transactions signatures
+        val stx = subFlow(CollectSignaturesFlow(tx, sessions))
+
+        //the finalityFlow finalizes the transaction calling the notary or notary Pool
+        return subFlow(FinalityFlow(stx, sessions))
+
     }
 }
 
+//Starts when the CarIssueIniatiorFlow is started
 @InitiatedBy(CarIssueInitiator::class)
-class CarIssueResponder(val counterpartySession: FlowSession) : FlowLogic<Unit>() {
+class CarIssueResponder(val counterpartySession: FlowSession) : FlowLogic<SignedTransaction>() {
     @Suspendable
-    override fun call() {
+    override fun call() : SignedTransaction{
         // Responder flow logic goes here.
+        // a object that implements the checkTransaction
+        // verify the logic that we want in the response flow
+        val signedTransactionFlow = object : SignTransactionFlow(counterpartySession){
+            override fun checkTransaction(stx: SignedTransaction) = requireThat {
+                val output = stx.tx.outputs.single().data
+                "The output must be a CarState" using (output is CarState)
+            }
+        }
+        val txWeJustSignedId = subFlow(signedTransactionFlow)
+        return subFlow(ReceiveFinalityFlow(counterpartySession, txWeJustSignedId.id))
     }
 }
